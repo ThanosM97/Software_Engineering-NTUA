1932fb20c8a4b1bd997daf2da0f07b32029b3e2f
PanosAntoniadis <pantoniadis97@gmail.com>
2019-01-04 02:30:40 +0200

    Just improve indentation

--- src/main/java/gr/ntua/ece/stingy/api/ProductsResource.java
+++ src/main/java/gr/ntua/ece/stingy/api/ProductsResource.java
@@ -16,103 +16,105 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/*
+ * A class implementing get and post methods of /products URI
+ */
 public class ProductsResource extends ServerResource {
 
-    private final DataAccess dataAccess = Configuration.getInstance().getDataAccess();
+	private final DataAccess dataAccess = Configuration.getInstance().getDataAccess();
 
-    @Override
-    protected Representation get() throws ResourceException {
-    	/**
-    	 * Get parameters of the get method.
-    	 */
-    	Form queryParams = getQuery();
-    	String startString = queryParams.getFirstValue("start");
-    	String countString = queryParams.getFirstValue("count");
-    	String status = queryParams.getFirstValue("status");
-    	String sort = queryParams.getFirstValue("sort");
-    	
-        Map<String, Object> map = new HashMap<>();
-        Limits limits = new Limits();
-        /**
-         * If a parameter exists and is valid put it in the map.
-         */
-        if (startString != null ) {
-        	Long start = null;
-            try {
-                start = Long.parseLong(startString);
-            }
-            catch(Exception e) {
-                throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "Invalid start: " + startString);
-            }
-            limits.setStart(start);
-        	map.put("start", start);
-        } else {
-        	/*
-        	 * default value for start is 0.
-        	 */
-        	map.put("start", 0);
-        }
-        if (countString != null ) {
-        	Long count = null;
-        	try {
-                count = Long.parseLong(countString);
-            }
-            catch(Exception e) {
-                throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "Invalid count: " + countString);
-            }
-            limits.setCount(count);
-        	map.put("count", count);
-        } else{
-        	/*
-        	 * default value for count is 20.
-        	 */
-        	map.put("count", 20);
-        }
-        /*
-         * get products based on the limits.
-         */
-        /*
-         * set default values for status and sort
-         */
-        if (status == null) {
-        	status = "ACTIVE";
-        }
-        if (sort == null) {
-        	sort = "id|DESC";
-        }
-        if (!status.equals("ALL") && !status.equals("WITHDRAWN") && !status.equals("ACTIVE")) {
-        	throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "Invalid status: " + status);
-        }
-        System.out.println(sort);
-        if (!sort.equals("id|ASC") && !sort.equals("id|DESC") && !sort.equals("name|ASC") && !sort.equals("name|DESC")) {
-        	throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "Invalid sort: " + sort);
-        }
-    	List<Product> products = dataAccess.getProducts(limits, status, sort);
-    	/*
-    	 * set current total products.
-    	 */
-        map.put("total", limits.getTotal());
-        map.put("products", products);
-        return new JsonMapRepresentation(map);
-    }
+	@Override
+	protected Representation get() throws ResourceException {
+		/**
+		 * Get parameters of the get method.
+		 */
+		Form queryParams = getQuery();
+		String startString = queryParams.getFirstValue("start");
+		String countString = queryParams.getFirstValue("count");
+		String status = queryParams.getFirstValue("status");
+		String sort = queryParams.getFirstValue("sort");
 
-    @Override
-    protected Representation post(Representation entity) throws ResourceException {
+		Map<String, Object> map = new HashMap<>();
+		Limits limits = new Limits();
+		/**
+		 * If a parameter exists and is valid put it in the map.
+		 */
+		if (startString != null ) {
+			Long start = null;
+			try {
+				start = Long.parseLong(startString);
+			}
+			catch(Exception e) {
+				throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "Invalid start: " + startString);
+			}
+			limits.setStart(start);
+			map.put("start", start);
+		} else {
+			/*
+			 * else put default value for start (0).
+			 */
+			map.put("start", 0);
+		}
+		if (countString != null ) {
+			Long count = null;
+			try {
+				count = Long.parseLong(countString);
+			}
+			catch(Exception e) {
+				throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "Invalid count: " + countString);
+			}
+			limits.setCount(count);
+			map.put("count", count);
+		} else{
+			/*
+			 * else put default value for count (20).
+			 */
+			map.put("count", 20);
+		}
+		/*
+		 * set default values for status and sort
+		 */
+		if (status == null) {
+			status = "ACTIVE";
+		}
+		if (sort == null) {
+			sort = "id|DESC";
+		}
+		if (!status.equals("ALL") && !status.equals("WITHDRAWN") && !status.equals("ACTIVE")) {
+			throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "Invalid status: " + status);
+		}
+		if (!sort.equals("id|ASC") && !sort.equals("id|DESC") && !sort.equals("name|ASC") && !sort.equals("name|DESC")) {
+			throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "Invalid sort: " + sort);
+		}
+		/*
+		 * get products based on the limits.
+		 */
+		List<Product> products = dataAccess.getProducts(limits, status, sort);
+		/*
+		 * set current total products.
+		 */
+		map.put("total", limits.getTotal());
+		map.put("products", products);
+		return new JsonMapRepresentation(map);
+	}
 
-        //Create a new restlet form
-        Form form = new Form(entity);
-        //Read the parameters
-        String name = form.getFirstValue("name");
-        String description = form.getFirstValue("description");
-        String category = form.getFirstValue("category");
-        boolean withdrawn = Boolean.valueOf(form.getFirstValue("withdrawn"));
-        String tagsString = form.getFirstValue("tags");
-        String extraDataString = form.getFirstValue("extraData");
+	@Override
+	protected Representation post(Representation entity) throws ResourceException {
 
-        //validate the values (in the general case)
-        //...
+		//Create a new restlet form
+		Form form = new Form(entity);
+		//Read the parameters
+		String name = form.getFirstValue("name");
+		String description = form.getFirstValue("description");
+		String category = form.getFirstValue("category");
+		boolean withdrawn = Boolean.valueOf(form.getFirstValue("withdrawn"));
+		String tagsString = form.getFirstValue("tags");
+		String extraDataString = form.getFirstValue("extraData");
 
-        Product product = dataAccess.addProduct(name, description, category, withdrawn, tagsString, extraDataString);
-        return new JsonProductRepresentation(product);
-    }
+		//validate the values (in the general case)
+		//...
+
+		Product product = dataAccess.addProduct(name, description, category, withdrawn, tagsString, extraDataString);
+		return new JsonProductRepresentation(product);
+	}
 }
