600f1723efc8aa00dd4944325b8b2be4c2091b32
Thanos Masouris <41332813+ThanosM97@users.noreply.github.com>
2019-01-05 20:43:46 +0200

    changed background color and added footer

--- src/main/webapp/pages/signup.js
+++ src/main/webapp/pages/signup.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import Head from 'next/head';
-import NavBar from '../components/NavBar.js'
+import NavBar from '../components/NavBar.js';
+import Footer from '../components/Footer.js';
 
 class SignUp extends Component {
 
   render() {
     return (
-      <div style={{backgroundColor:"#e5e5e5", position:"absolute",height:"100%", width:"100%"}}>
+      <div style={{backgroundColor:"#f1f1f1", position:"absolute",height:"100%", width:"100%"}}>
         <Head>
           <title>Εγγραφή χρήστη | Stingy </title>
           <link rel="shortcut icon" href="../static/logo/logo.png"/>
-          <link href="/static/signupStyle.css" rel="stylesheet" />
+          <link href="../static/signupStyle.css" rel="stylesheet" />
         </Head>
         <NavBar />
         <div className="modal">
           <h1>Εγγραφή νέου χρήστη</h1>
 
         </div>
+        <Footer />
       </div>
     );
   }
