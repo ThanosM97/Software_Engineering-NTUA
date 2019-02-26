0732e029abf2c3f8a803e193057d6b9133adaf1f
PanosAntoniadis <pantoniadis97@gmail.com>
2019-02-26 19:00:50 +0200

    Fix all compilation errors

--- src/main/sql/database.sql
+++ src/main/sql/database.sql
@@ -159,7 +159,7 @@ Smartphones have the following extra data: CPU cores, cpu freq, RAM, capacity, s
 ,'Laptop, Windows 10','Intel Celeron N4000, 4GB, 500GB, Windows 10, 15.6, Intel HD Graphics 600'
 'Laptop, Windows 10', 'AMD A-Series, 4GB, 128GB,Windows 10, 14,  Radeon R5'
  'Black, Dual Sim, Android','Octa core, 1.8GHz, 3GB, 32GB, 6.2, 12 Mp'
-*.
+*/
 LOCK TABLES `Product` WRITE;
 /*!40000 ALTER TABLE `Product` DISABLE KEYS */;
 INSERT INTO `Product` VALUES (1,'SAMSUNG TV 43\'\' UE43NU7122','Μινιμαλιστικό design και ποιότητα κατασκευής, 4Κ ανάλυση, Smart εφαρμογές και πρωτοποριακές τεχνολογίες για ακόμη ελκυστικότερη εικόνα στην πιο προσιτή τιμή','TV','\0', NULL),(2,'LG TV 49\'\' 49UK6200','Με ανάλυση UHD 4K, τεχνολογία 4K Active HDR, «έξυπνο» λειτουργικό webOS και ήχοUltra Surround για ανεπανάληπτη εμπειρία θέασης','TV','\0', NULL),(3,'DELL Laptop Inspiron 3573', 'Laptop Dell Inspiron 3573 με επεξεργαστή Intel Celeron N4000, μνήμη RAM 4GB, σκληρό δίσκο 500GB και HD αντι-ανακλαστική οθόνη 15.6 ιντσών, ιδανικό για καθημερινή χρήση.', 'Laptop','\0', NULL),(4,' Lenovo Laptop IdeaPad 330S-14AST', 'Αν ψάχνεις έναν υπερπλήρη φορητό ηλεκτρονικό υπολογιστή, για υψηλής απόδοσης εφαρμογές multimedia, ήχο και επεξεργαστική ισχύ, τότε το Lenovo IdeaPad 330S-14AST είναι το ιδανικό Laptop για εσένα!', 'Laptop', '\0', NULL),(5,' Xiaomi Smartphone Redmi Note 6 Pro', 'Το Redmi Note 6 Pro διαθέτει διπλή εμπρόσθια και πίσω κάμερα που κάνει τη διαφορά, ενσωματώνει τον Snapdragon 636, τον νεότερο 14nm επεξεργαστή της Qualcomm, βελτιώνοντας σημαντικά τις συνολικές επιδόσεις και την ενεργειακή αποτελεσματικότητα. Επιπλέον, τα 3GB μνήμη RAM συνεισφέρουν στην ομαλή λειτουργία, ενώ η μπαταρία των 4000mAh θα καλύψει κάθε σου ανάγκη.', 'Smartphone', '\0', NULL);
@@ -170,13 +170,15 @@ UNLOCK TABLES;
 --
 -- Dumping data for table `Shop`
 --
+/*
 , 'computing, laptops, TV'
 , 'laptops, TV, monitors'
 , 'laptops, TV'
+*/
 LOCK TABLES `Shop` WRITE;
 /*!40000 ALTER TABLE `Shop` DISABLE KEYS */;
-INSERT INTO `Shop` VALUES (1, 'Πλαίσιο Αθήνα', 'Βουλής 3,10562,Αθήνα', 23.732913, 37.977344, '\0'),(2, 'Κωτσόβολος Αθήνα','Σταδίου 34 & Κοραή,10564,Αθήνα', 23.732025,37.979795, '\0'),(3, 'Public Αθήνα', 'Καραγεώργη Σερβίας 1,10563,Αθήνα', 23.733666 ,37.976454, '\0');
-/*!40000 ALTER TABLE `shop` ENABLE KEYS */;
+INSERT INTO `Shop` VALUES (1, 'Πλαίσιο Αθήνα', 'Βουλής 3,10562,Αθήνα', 23.732913, 37.977344, NULL, '\0'),(2, 'Κωτσόβολος Αθήνα','Σταδίου 34 & Κοραή,10564,Αθήνα', 23.732025,37.979795, NULL, '\0'),(3, 'Public Αθήνα', 'Καραγεώργη Σερβίας 1,10563,Αθήνα', 23.733666 ,37.976454, NULL, '\0');
+/*!40000 ALTER TABLE `Shop` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -194,7 +196,7 @@ UNLOCK TABLES;
 --
 LOCK TABLES `extraData` WRITE;
 /*!40000 ALTER TABLE `extraData` DISABLE KEYS */;
-INSERT INTO `extraData` VALUES (1, 'Resolution', '4K', 1), (2, 'Smart', 'Yes', 1), (3, 'Frequency', '1300 PQI', 1), (4, 'Resolution', '4K', 2), (5, 'Smart', 'Yes', 2), (6, 'Frequency', '1500 PMI', 2), (7, 'CPU', 'Intel Celeron N4000', 3), (8, 'RAM', '4GB', 3), (9, 'Hard Drive', '500GB', 3), (10, 'OS', 'Windows 10', 3), (10, 'Graphics Card', 'Intel HD Graphics 600', 3), (11, 'CPU', 'AMD A-Series', 4), (12, 'RAM', '4GB', 4), (13, 'Hard Drive', '128GB', 4), (14, 'OS', 'Windows 10', 4), (15, 'Graphics Card', 'Radeon R5', 4), (16, 'Number of Cores', 'Octa Core', 5), (17, 'CPU frequency', '1.8GHz', 5), (18, 'RAM', '3GB', 5), (19, 'Capacity', '32GB', 5), (20, 'Camera', '12Mp', 5);
+INSERT INTO `extraData` VALUES (1, 'Resolution', '4K', 1), (2, 'Smart', 'Yes', 1), (3, 'Frequency', '1300 PQI', 1), (4, 'Resolution', '4K', 2), (5, 'Smart', 'Yes', 2), (6, 'Frequency', '1500 PMI', 2), (7, 'CPU', 'Intel Celeron N4000', 3), (8, 'RAM', '4GB', 3), (9, 'Hard Drive', '500GB', 3), (10, 'OS', 'Windows 10', 3), (11, 'Graphics Card', 'Intel HD Graphics 600', 3), (12, 'CPU', 'AMD A-Series', 4), (13, 'RAM', '4GB', 4), (14, 'Hard Drive', '128GB', 4), (15, 'OS', 'Windows 10', 4), (16, 'Graphics Card', 'Radeon R5', 4), (17, 'Number of Cores', 'Octa Core', 5), (18, 'CPU frequency', '1.8GHz', 5), (19, 'RAM', '3GB', 5), (20, 'Capacity', '32GB', 5), (21, 'Camera', '12Mp', 5);
 /*!40000 ALTER TABLE `extraData` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -221,7 +223,7 @@ UNLOCK TABLES;
 --
 LOCK TABLES `User` WRITE;
 /*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` VALUES (
+INSERT INTO `User` VALUES (1, 'panos', '1234', 'panos', 'anto', 'asdasd', 'asdasd', 'asdas', '123', 0, NULL);
 /*!40000 ALTER TABLE `User` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -230,8 +232,8 @@ UNLOCK TABLES;
 --
 LOCK TABLES `Record` WRITE;
 /*!40000 ALTER TABLE `Record` DISABLE KEYS */;
-INSERT INTO `Record` VALUES (1, 300, '2018-12-12', 1, 1), (2, 250, '2018-12-20', 1, 2), (3, 280, '2018-11-15', 2, 1), (4, 220,'2018-11-29', 2, 3), (5, 100, '2018-10-10', 3,2),(6,250,'2018-10-11', 3,3);
-/*!40000 ALTER TABLE `record` ENABLE KEYS */;
+INSERT INTO `Record` VALUES (1, 300, '2018-12-12', NULL, 1, 1, 1), (2, 250, '2018-12-20', NULL, 1, 2, 1), (3, 280, '2018-11-15', NULL, 2, 1, 1), (4, 220,'2018-11-29', NULL, 2, 3, 1), (5, 100, '2018-10-10', NULL, 3,2, 1),(6,250,'2018-10-11', NULL, 3,3, 1);
+/*!40000 ALTER TABLE `Record` ENABLE KEYS */;
 UNLOCK TABLES;
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
