639ca0bcfed27ef43f060f0365d798e3bd10281f
arkountos <eternallistener@gmail.com>
2019-02-28 15:02:36 +0200

    Product list page and css

--- src/main/webapp/components/ProductList.js
+++ src/main/webapp/components/ProductList.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {ProductViewSpecs} from './ProductView.js';
+
+const smartphone = "../static/images/smartphone250x250.png";
 
 export class ProductList extends React.Component{
     render(){
         const product_list = this.props.products.map(product => 
             <div class='product-entry'>
-                <a>This will be a product!</a>
-                <ProductViewSpecs class='main-product' product={product}/>
+                <img src={smartphone} class='product-image' />
+                <a class='product-name'>Nokia L330 under triangle radar</a><br></br>
+                <a class='product-price'>Χαμηλότερη καταγεγραμμένη τιμή: {product.price}</a>
+                <a class='product-specs'>Χαρακτηριστικά: Οθόνη:{product.screen} RAM:{product.ram} ROM:{product.rom}</a>
             </div>
             );
         // Maybe use the same Component for displaying specs here? We'll see
         return(
-            <div>
-                  <div>{product_list}</div>
+            <div >
+                  <div class='productbox'>{product_list}</div>
             </div>
         );
     }
--- src/main/webapp/pages/productlist.js
+++ src/main/webapp/pages/productlist.js
@@ -5,20 +5,24 @@ import Footer from '../components/Footer';
 // I'm going to assume that We get the data at some point to this format
 const products = [
     {device: 'smartphone', name: 'Testphone', screen: '5"', ram: '4GB', rom: '64GB', backCamera: '20Mp', 
-    frontCamera: '4Mp', cpuCores: '4', manufacturer: 'Samsung'},
+    frontCamera: '4Mp', cpuCores: '4', manufacturer: 'Samsung', price: '300'},
     {device: 'smartphone', name: 'Exphone', screen: '4"', ram: '2GB', rom: '64GB', backCamera: '20Mp', 
-    frontCamera: '4Mp', cpuCores: '2', manufacturer: 'Fiphone'},
+    frontCamera: '4Mp', cpuCores: '2', manufacturer: 'Fiphone', price: '300'},
     {device: 'smartphone', name: 'RADPHONE', screen: '18"', ram: '9TB', rom: '129GB', backCamera: '20Mp', 
-    frontCamera: '100Mp', cpuCores: '1000', manufacturer: 'RADPHONES COMPANY'}
+    frontCamera: '100Mp', cpuCores: '1000', manufacturer: 'RADPHONES COMPANY', price: '400'}
 ]
 
 var content = (
     <div>
-            <NavBar />
-            <h1>Coming Soon :)</h1>
-            <a>Related Products:</a>
-            <ProductList products={products} />
-            <Footer />
+        <head>
+            <link href='../static/ProductList.css' type='text/css' rel='stylesheet' />
+        </head>
+        <div class='background'>
+                <NavBar />
+                <h3>Related Products:</h3>
+                <ProductList products={products} />
+                <Footer />
+        </div>
     </div>
 );
 export default () => content;
