cb1fe705d3db348349a38140b845f1969374f0d1
ThanosM97 <41332813+ThanosM97@users.noreply.github.com>
2019-03-05 17:45:53 +0200

    Connection with API

--- src/main/webapp/components/ProductView.js
+++ src/main/webapp/components/ProductView.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Map from './Map';
-//import { NONAME } from 'dns';
 
+const fetch = require("node-fetch");
 
 const smartphone = "../static/images/smartphone250x250.png";
 const like = "../static/images/like.png";
@@ -10,237 +10,166 @@ const unlike = "../static/images/unlike.png";
 const questionmark = "../static/images/questionmark.png";
 
 
-export class ProductView extends React.Component{
-    render(){
-        // We should give as the product argument the 'category' argument from the URL
-        // For testing I am going to assume the URL is of type www.stingy.com/category='category'&
-        return(
-            <div>
-                <ProductViewSpecs class='main-product' product={this.props.product}/>  
-                <ProductViewShopList class='main-shops' shops={this.props.shops}/>
-            </div>
-        );
-    }
-}
+class ProductViewSpecs extends React.Component{
+  constructor(props){
+    super(props)
+  }
 
-export class ProductViewSpecs extends React.Component{
-    render(){
-        let product_render;
-        switch (this.props.product.device){
-            case 'smartphone':
-                product_render = ( <ProductViewSmartphone product={this.props.product} />);
-                break;
-            case 'tv':
-                product_render = ( <ProductViewTv product={this.props.product} />);
-                break;
-            case 'laptop':
-                product_render = ( <ProductViewLaptop product={this.props.product} />);
-                break;
-            case 'tablet':
-                product_render = ( <ProductViewTablet product={this.props.product} />);
-                break;
-            case 'monitor':
-                product_render = ( <ProductViewMonitor product={this.props.product} />);
-                break;
-            default:
-                product_render = ( <div> Oops! Invalid product type :( </div> );
-            }
+  getExtraData(product){
+    var len = Object.keys(product.extraData).length
+    //Array.from(Array(len).keys) creates array [0,1,2,3,...,len] and then we iterate over it
+    let result = Array.from(Array(len).keys()).map(i =>
+      <tr key={i}>
+        <td key={i+5}>{Object.keys(product.extraData)[i] + ":  " + Object.values(product.extraData)[i] }</td>
+      </tr>)
 
-        return(
-                <div class='specs'>
-                    <div>{product_render}</div>
-                </div>
-        );
-    }
-}
+    return result
+  }
 
-class ProductViewSmartphone extends React.Component{
-    render(){
-        return(
-            <div>
-                <a class='product-name'>{this.props.product.name}</a><br />
-                <img src={smartphone} class='product-image' /><br />
-                <table class='specs-table'>
-                    <tr>
-                        <td>Λειτουργικό Σύστημα:</td> 
-                        <td>{this.props.product.OS}</td>
-                    </tr>
-                    <tr>
-                        <td>Οθόνη:</td> 
-                        <td>{this.props.product.size}</td>
-                    </tr>
-                    <tr>
-                        <td>Μνήμη RAM:</td> 
-                        <td>{this.props.product.ram}</td>
-                    </tr>
-                    <tr>
-                        <td>Χωρητικότητα:</td> 
-                        <td>{this.props.product.capacity}</td>
-                    </tr>
-                    <tr>
-                        <td>Κάμερα:</td> 
-                        <td>{this.props.product.backCamera}</td>
-                    </tr>
-                    <tr>
-                        <td>Selfie:</td> 
-                        <td>{this.props.product.frontCamera}</td>
-                    </tr>
-                    <tr>
-                        <td>Πυρήνες Επεξεργαστή:</td> 
-                        <td>{this.props.product.cpuCores}</td>
-                    </tr>
-                    <tr>
-                        <td>Ταχύτητα Επεξεργαστή:</td> 
-                        <td>{this.props.product.cpuFrequency}</td>
-                    </tr>
-                </table>
-            </div>
-        );
-    }
-}
 
-class ProductViewTv extends React.Component{
-    render(){
-        return(
-            <div>
-                <a class='product-name'>{this.props.product.name}</a><br />
-                <img src={smartphone} class='product-image' /><br />
-                <table class='specs-table'></table>
-                    <tr>
-                        <td>4K:</td>
-                        <td>{this.props.product.fourk}</td>
-                    </tr>
-                    <tr>
-                        <td>Smart:</td>
-                        <td>{this.props.product.smart}</td>
-                    </tr>
-                    <tr>
-                        <td>Συχνότητα:</td>
-                        <td>{this.props.product.frequency}</td>
-                    </tr>
-            </div>
-        );
-    }
-}
-
-class ProductViewLaptop extends React.Component{
-	render(){
-		return(
-            <div>
-                <a class='product-name'>{this.props.product.name}</a><br />
-                <img src={laptop} class='product-image' /><br />
-                <table class='specs-table'></table>
-                    <tr>
-                        <td>CPU:</td>
-                        <td>{this.props.product.cpu}</td>
-                    </tr>
-                    <tr>
-                        <td>RAM:</td>
-                        <td>{this.props.product.ram}</td>
-                    </tr>
-                    <tr>
-                        <td>Σκληρός Δίσκος:</td>
-                        <td>{this.props.product.harddrive}</td>
-                    </tr>
-                    <tr>
-                        <td>Λειτουργικό Σύστημα:</td>
-                        <td>{this.props.product.OS}</td>
-                    </tr>
-                    <tr>
-                        <td>Μέγεθος Οθόνης:</td>
-                        <td>{this.props.product.screensize}</td>
-                    </tr>
-                    <tr>
-                        <td>Κάρτα Γραφικών:</td>
-                        <td>{this.props.product.graphicscard}</td>
-                    </tr>
-            </div>
-        )
-	}
-}
-
-class ProductViewMonitor extends React.Component{
-    render(){
-		return(
-            <div>
-                <a class='product-name'>{this.props.product.name}</a><br />
-                <img src={laptop} class='product-image' /><br />
-                <table class='specs-table'></table>
-                    <tr>
-                        <td>Μέγεθος οθόνης:</td>
-                        <td>{this.props.product.screensize}</td>
-                    </tr>
-                    <tr>
-                        <td>Ανάλυση:</td>
-                        <td>{this.props.product.resolution}</td>
-                    </tr>
-            </div>
-        )
-    }
-}
-
-class ProductViewTablet extends React.Component{
-    render(){
-		return(
-            <div>
-                <a class='product-name'>{this.props.product.name}</a><br />
-                <img src={laptop} class='product-image' /><br />
-                <table class='specs-table'></table>
-                    <tr>
-                        <td>Μέγεθος οθόνης:</td>
-                        <td>{this.props.product.screensize}</td>
-                    </tr>
-                    <tr>
-                        <td>RAM:</td>
-                        <td>{this.props.product.ram}</td>
-                    </tr>
-                    <tr>
-                        <td>Λειτουργικό:</td>
-                        <td>{this.props.product.os}</td>
-                    </tr>
-                    <tr>
-                        <td>Σκληρός Δίσκος:</td>
-                        <td>{this.props.product.harddrive}</td>
-                    </tr>
-            </div>
-        )
-    }
+  render(){
+    let product = this.props.product;
+    let extraData = this.getExtraData(product);
+      return(
+        <div className="specs">
+            <a class='product-name'>{product.name}</a><br />
+            <img src={product.image} class='product-image' /><br />
+            <table className='specs-table'>
+              {extraData}
+            </table>
+        </div>
+      );
+  }
 }
 
 class ProductViewShopList extends React.Component{
-    
+
     constructor(props){
         super(props);
         this.state = {
-            display: 'none'
+            display: 'none',
+            records:null,
+            total:null,
+            count:6,
+            start:0,
+            filters:{
+              geoDist:null,
+              geoLng:null,
+              geoLat:null,
+              dateFrom:null,
+              dateTo:null,
+            },
+            filtersDiv:null,
+            position:null,
+            sliderValue:0,
         };
         this.handleClick = this.handleClick.bind(this);
+        this.showFilters = this.showFilters.bind(this);
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    componentDidMount(){
+      navigator.geolocation.getCurrentPosition(position => {
+          this.setState({position:position.coords});
+          let data = fetch('https://localhost:8765/observatory/api/prices?products='+this.props.productId+"&geoLat="+position.coords.latitude+"&geoLng="+position.coords.longitude+"&geoDist=50").then((resp)=>{
+            resp.json().then((res)=>{
+              this.setState({records:res.prices, total:res.total});
+            })
+          })
+      });
+
     }
 
     handleClick(e) {
         const newDisplay = this.state.display == 'none' ? e.target.name : 'none';
         this.setState({display: newDisplay});
     }
-    
+
+    handleChange(e) {
+      console.log(e.target.value)
+      this.setState({sliderValue:e.target.value})
+    }
+
+    showFilters(){
+      let curDate = new Date();
+      curDate.hours = curDate.hours + 2;
+      curDate = curDate.toISOString().split('T')[0]
+      let filters = (
+        <div className="filtersDiv">
+          <form id="filters">
+            <div className="DistanceFilter" style={{marginLeft:"10px"}}>
+              <label>Απόσταση από το κατάστημα:</label><br /><br />
+              <input id="slider" type="range" min="0" max="3" defaultValue="3" onChange={this.handleChange}  step="1"/>
+              <div id="sliderTags" >
+                <h4> 2χλμ </h4>
+                <h4> 5χλμ </h4>
+                <h4> 10χλμ </h4>
+                <h4> Όλα </h4>
+              </div>
+            </div>
+            <div className= "DistanceFilter" align="center" style={{marginLeft:"4%"}}>
+              <label> Από ημερομηνία: </label> <br /><br />
+              <input type="date" defaultValue={curDate} />
+            </div>
+            <div className= "DistanceFilter" align="center">
+              <label> Έως ημερομηνία: </label> <br /><br />
+              <input type="date" defaultValue={curDate}/>
+            </div>
+          </form>
+        </div>
+      )
+      if (this.state.filtersDiv) this.setState({filtersDiv:null})
+      else this.setState({filtersDiv:filters})
+    }
+
     render(){
-        const shoplist = this.props.shops.map((shop, i) => 
-            <div class='shopbox' key={i}>
-                <as class='shopname'>{shop.name} </as>
-                <as class='price'>{shop.price} </as><br></br> 
-                <as>Άλλα στοιχεία: {shop.otherData} </as><br></br>
-                <as>Αξιοπιστία καταχώρησης: <img src={like} class='like-img'/> <img src={unlike} class='like-img'/><img src={questionmark} class='questionmark-img'/></as><br></br>
-                <div>    
-                    <button href='#' class='address' name={'button_' + i} onClick={this.handleClick}>Αλέκου Παναγούλη 9, Νίκαια </button><br></br>
-                    {this.state.display == 'button_' + i ? <Map lat={37.947425} lng={23.664146}/> : <a></a>}
+      if (this.state.records){
+      console.log(this.state.records["0"], this.state.total)
+      let records = this.state.records;
+        const shoplist = records.map((record, i) =>
+            <div className='shopbox' key={i}>
+                <as className='shopname'>{record.shopName} </as>
+                <as className='price'>{record.price} &euro;</as><br/><br/>
+                <as style={{float:"left"}}>Απόσταση καταστήματος: {record.shopDist / 1000}χλμ. </as><br/>
+                <as style={{float:"left"}}>Αξιοπιστία καταχώρησης: <img src={like} className='like-img'/> <img src={unlike} className='like-img'/><img src={questionmark} className='questionmark-img'/></as><br/><br/>
+                <div>
+                    <button href='#' className='address' name={'button_' + i} onClick={this.handleClick}>{record.shopAddress}</button><br></br>
+                    {this.state.display == 'button_' + i ? <Map lat={record.lat} lng={record.lng}/> : <a></a>}
                 </div>
             </div>
-            );
+          );
         return(
             <div>
-                <h3>Διαθέσιμο στα εξής Καταστήματα:</h3>
-                <div class='myshoplist'>{shoplist}</div>
+                <div className="avail">
+                  <h3>Διαθέσιμο στα εξής αταστήματα:</h3>
+                  <button className="filtersButton" onClick={this.showFilters} style={{width:"10%"}}> Show Filters </button>
+                </div>
+                                  {this.state.filtersDiv}
+                <div className="myshoplist" align="center">
+                  {shoplist}
+                </div>
             </div>
-        )
+        );
+      } else {
+        return( <div> Waiting for data... </div>)
+      }
     }
 }
 
+
+export class ProductView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render(){
+      let product = this.props.product;
+        return(
+            <div>
+              <ProductViewSpecs class='main-product' product={product}/>
+              <ProductViewShopList class='main-shops' productId={product.id}/>
+            </div>
+        );
+      }
+}
--- src/main/webapp/pages/productview.js
+++ src/main/webapp/pages/productview.js
@@ -1,10 +1,17 @@
 import {ProductView} from '../components/ProductView.js';
+import React, { Component } from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import Router from 'next/router';
+import Head from 'next/head';
+import Cookies from 'universal-cookie';
 
+const cookies = new Cookies();
+const querystring = require('querystring');
+const fetch = require("node-fetch");
 
 const myExampleProduct = {
-    device: 'smartphone', name: 'Testphone 5inch 4GB RAM Black', size: '6"', ram: '4GB', capacity: '32GB', backCamera: '20Mp', 
+    device: 'smartphone', name: 'Testphone 5inch 4GB RAM Black', size: '6"', ram: '4GB', capacity: '32GB', backCamera: '20Mp',
     frontCamera: '4Mp', cpuCores: '4', cpuFrequency: '1.8 GHz', OS: 'Android'
 };
 
@@ -14,16 +21,50 @@ const myExampleShops = [
     {name: 'Πλαίσιο Πειραιά', price: '1000$', otherData: ''}
 ];
 
-var content = (
-    <div>
-        <head>
-            <link href='../static/ProductView.css' type='text/css' rel='stylesheet' />
-        </head>
+class Productview extends Component {
+  constructor(){
+    super()
+    this.state={
+      loggedIn:false,
+      query:"",
+      data:null,
+    }
+  }
+
+  componentWillMount(){
+    if (cookies.get('auth'))  this.setState({loggedIn:true})
+  }
+
+  componentDidMount(){
+    this.setState({query:Router.query});
+    let myQuery = querystring.stringify(Router.query);
+    myQuery = myQuery.replace("id=","")
+    let data = fetch('https://localhost:8765/observatory/api/products/'+myQuery).then((resp)=>{
+      resp.json().then((res)=>{
+        this.setState({data:res});
+      })
+    })
+  }
+
+  render(){
+    let product = this.state.data;
+    if (product){
+      return(
         <div>
-            <NavBar />
-            <ProductView product={myExampleProduct} shops={myExampleShops} />
-            <Footer />
+            <head>
+                <link href='../static/ProductView.css' type='text/css' rel='stylesheet' />
+            </head>
+            <div>
+                <NavBar />
+                <ProductView product={this.state.data} shops={myExampleShops} />
+                <Footer />
+            </div>
         </div>
-    </div>
-);
-export default () => content;
+      );
+    }
+    return(
+      <div> Waiting for data...</div>
+    );
+  }
+}
+export default Productview
