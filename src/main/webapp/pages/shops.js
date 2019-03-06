9142ec504b771263398060e7a9cb0259db6b77d1
ThanosM97 <41332813+ThanosM97@users.noreply.github.com>
2019-03-06 06:31:41 +0200

    Added button for shop deletion and sort functionality

--- src/main/webapp/pages/shops.js
+++ src/main/webapp/pages/shops.js
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import NavBar from '../components/NavBar.js';
 import Footer from '../components/Footer.js';
 import Cookies from 'universal-cookie';
+import Router from 'next/router'
+
 const fetch = require("node-fetch");
 
 const cookies = new Cookies();
@@ -27,8 +29,16 @@ class Shops extends Component {
       pagesNo:0,
       activePage:1,
       loggedIn:false,
+      toDelete:null,
+      button:null,
+      visibility:"hidden",
+      buttonVisibility:"hidden",
+      sort:false,
+
     }
     this.handleChange = this.handleChange.bind(this);
+    this.deleteShop = this.deleteShop.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentWillMount(){
@@ -36,6 +46,7 @@ class Shops extends Component {
   }
 
   componentDidMount(){
+    if (cookies.get('auth')) this.setState({buttonVisibility:"visible"})
     let data = fetch('https://localhost:8765/observatory/api/shops?start='+this.state.start+"&count="+this.state.count).then((resp)=>{
       resp.json().then((res)=>{
         this.setState({data:res.shops, total:res.total, pagesNo:Math.ceil(res.total/this.state.count)});
@@ -46,12 +57,39 @@ class Shops extends Component {
 
   handleChange(event){
     let st = (event.target.id -1) * (this.state.count +1);
-    let data = fetch('https://localhost:8765/observatory/api/shops?start='+st+"&count="+this.state.count).then((resp)=>{
+    var sort="";
+    if(event.target.name == "sort" || this.state.sort){st=0 ; sort="&sort=name|ASC"; this.setState({sort:true})}
+    let data = fetch('https://localhost:8765/observatory/api/shops?start='+st+"&count="+this.state.count+sort).then((resp)=>{
       resp.json().then((res)=>{
         this.setState({data:res.shops, total:res.total, pagesNo:Math.ceil(res.total/this.state.count)});
       })
     })
     this.setState({activePage: event.target.id,start:st});
+  }
+
+  handleClick(event){
+    this.setState({visibility:"visible", toDelete: event.target.id});
+  }
+
+  dontDelete(){
+    Router.back()
+  }
+
+  deleteShop(e){
+    if (e.target.value="yes"){
+      let cookie = cookies.get('auth');
+      fetch("https://localhost:8765/observatory/api/shops/"+this.state.toDelete,{
+        method: "delete",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "X-OBSERVATORY-AUTH": cookie
+        },
+      }).then(response => {
+          if (!response.ok) { alert("Something went wrong!"); throw response }
+          else {alert("Shop has been deleted"); Router.push('/shops')}
+
+        })
+      } else { Router.push('/shops')}
   }
 
 
@@ -61,6 +99,7 @@ class Shops extends Component {
             <img src={shop.image} className='product-image' />
             <h2>{shop.name}</h2>
             <h4>{shop.address}</h4>
+            <button id={shop.id} style={{visibility:this.state.buttonVisibility}}className="deleteButton" onClick={this.handleClick}> Delete shop?</button>
         </div>
         );
 
@@ -69,6 +108,13 @@ class Shops extends Component {
         i == this.state.activePage ? pages.push(<Page value={i} active="active" changeHandler={this.handleChange}/>) : pages.push(<Page value={i} active="" changeHandler={this.handleChange} />)
         }
 
+        const confirm = (
+          <div style={{visibility:this.state.visibility, position:"fixed", top:"50%", left:"35%",zIndex:10,backgroundColor:"#f2f2f2", border:"2px solid red",padding:"2%"}}>
+            <h2> Are you sure that you want to delete this shop? </h2>
+            <button  className="buttonConf red" onClick={this.deleteShop} value="yes"> Yes</button>
+            <button className="buttonConf blue"  onClick={this.dontDelete} value="no"> No</button>
+          </div>
+        )
 
     return (
       <div style={{backgroundColor:"#f1f1f1", position:"relative", width:"100%" , minHeight:"100vh", display:"block", overflow:"hidden"}} align="center">
@@ -78,9 +124,10 @@ class Shops extends Component {
           <link href="../static/shopsStyle.css" rel="stylesheet" />
         </Head>
         <NavBar loggedIn={this.state.loggedIn} />
+        {confirm}
         <div>
           <h1 style={{marginTop:"2%"}}> Καταστήματα </h1>
-          <button className="filter"> Αλφαβητική σειρά </button>
+          <button className="filter" name="sort" onClick={this.handleChange}> Αλφαβητική σειρά </button>
         </div>
         <div  id="horizontalLine" align="center"></div>
         <div style={{width:"80%",marginTop:"2%", position:"relative", display:"block"}}>
